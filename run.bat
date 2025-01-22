c6a0f7637d2104f2bd5bb9d8b806d91b45b95551
dugumoss <here1271@163.com>
2025-01-22 09:16:29 +0800

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 cls
-start "D:\Program Files (x86)\Tencent\QQBrowser" "http://127.0.0.1:5000"
+start "http://127.0.0.1:5000"
 cmd /k  ".\venv\Scripts\activate && .\venv\Scripts\python.exe app.py"
 
